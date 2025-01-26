d7dce99fb5c37cce60c1b4463fbbde2bc87379ae
Max Nguyen <trung.nguyenhoangminh1998@gmail.com>
2025-01-27 01:22:34 +0700

    fix: prevent conflict with tmux leader on mac

--- wezterm.lua
+++ wezterm.lua
@@ -40,65 +40,76 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 	config.default_prog = { "cmd.exe", "/s", "/k", initBat }
 end
 
-config.keys = {
-	-- CTRL-SHIFT-l activates the debug overlay
-	{ key = "L", mods = "CTRL", action = wezterm.action.ShowDebugOverlay },
-	-- Split horizontal
-	{
-		key = "|",
-		mods = "LEADER|SHIFT",
-		action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
-	},
-	-- Split Vertical
-	{ key = "-", mods = "LEADER|SHIFT", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
-	-- Move between panes
-	{ key = "h", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
-	{ key = "j", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
-	{ key = "k", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
-	{ key = "l", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
-	-- Switch to new or existing workspace
-	-- Similar to when you attach to or switch tmux sessions
-	{
-		key = "W",
-		mods = "LEADER|SHIFT",
-		action = wezterm.action.PromptInputLine({
-			description = wezterm.format({
-				{ Attribute = { Intensity = "Bold" } },
-				{ Foreground = { AnsiColor = "Fuchsia" } },
-				{ Text = "Enter name for new workspace." },
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	config.leader = {
+		key = "\\",
+		mods = "CTRL",
+		timeout_milliseconds = 1000,
+	}
+	config.keys = {
+		-- CTRL-SHIFT-l activates the debug overlay
+		{ key = "L", mods = "CTRL", action = wezterm.action.ShowDebugOverlay },
+		-- Split horizontal
+		{
+			key = "|",
+			mods = "LEADER|SHIFT",
+			action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
+		},
+		-- Split Vertical
+		{
+			key = "-",
+			mods = "LEADER|SHIFT",
+			action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }),
+		},
+		-- Move between panes
+		{ key = "h", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+		{ key = "j", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+		{ key = "k", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+		{ key = "l", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+		-- Switch to new or existing workspace
+		-- Similar to when you attach to or switch tmux sessions
+		{
+			key = "W",
+			mods = "LEADER|SHIFT",
+			action = wezterm.action.PromptInputLine({
+				description = wezterm.format({
+					{ Attribute = { Intensity = "Bold" } },
+					{ Foreground = { AnsiColor = "Fuchsia" } },
+					{ Text = "Enter name for new workspace." },
+				}),
+				action = wezterm.action_callback(function(window, pane, line)
+					-- line will be `nil` if they hit escape without entering anything
+					-- An empty string if they just hit enter
+					-- Or the actual line of text they wrote
+					if line then
+						window:perform_action(
+							wezterm.action.SwitchToWorkspace({
+								name = line,
+							}),
+							pane
+						)
+					end
+				end),
 			}),
-			action = wezterm.action_callback(function(window, pane, line)
-				-- line will be `nil` if they hit escape without entering anything
-				-- An empty string if they just hit enter
-				-- Or the actual line of text they wrote
-				if line then
-					window:perform_action(
-						wezterm.action.SwitchToWorkspace({
-							name = line,
-						}),
-						pane
-					)
-				end
-			end),
-		}),
-	},
-}
+		},
+	}
 
--- Create a status bar on the top right that shows the current workspace and date
-wezterm.on("update-right-status", function(window, _)
-	local date = wezterm.strftime("%d-%m-%Y %H:%M:%S")
+	-- Create a status bar on the top right that shows the current workspace and date
+	wezterm.on("update-right-status", function(window, _)
+		local date = wezterm.strftime("%d-%m-%Y %H:%M:%S")
 
-	-- Make it italic and underlined
-	window:set_right_status(wezterm.format({
-		{ Attribute = { Underline = "Single" } },
-		{ Attribute = { Italic = true } },
-		{ Attribute = { Intensity = "Bold" } },
-		{ Foreground = { AnsiColor = "Fuchsia" } },
-		{ Text = window:active_workspace() },
-		{ Text = "   " },
-		{ Text = date },
-	}))
-end)
+		-- Make it italic and underlined
+		window:set_right_status(wezterm.format({
+			{ Attribute = { Underline = "Single" } },
+			{ Attribute = { Italic = true } },
+			{ Attribute = { Intensity = "Bold" } },
+			{ Foreground = { AnsiColor = "Fuchsia" } },
+			{ Text = window:active_workspace() },
+			{ Text = "   " },
+			{ Text = date },
+		}))
+	end)
+end
 
 local font_family = "JetBrainsMono Nerd Font"
 local font = wezterm.font({
@@ -118,12 +129,6 @@ config.inactive_pane_hsb = {
 	hue = 0.5,
 	saturation = 0.5,
 	brightness = 0.6,
-}
-
-config.leader = {
-	key = "\\",
-	mods = "CTRL",
-	timeout_milliseconds = 1000,
 }
 
 -- and finally, return the configuration to wezterm
